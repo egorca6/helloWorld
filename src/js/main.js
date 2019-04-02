30166485bb02e7f57c21c81d6cff9f0e9cba016b
egor <egorus88@yahoo.com>
2019-04-02 21:35:12 +0300

    tea

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,18 @@
 alert('hello world');
+function test() {
+var eng = ['green', 'yellow', 'red', 'black'];
+var rus = ['зеленый', 'желтый','красный', 'черный'];
 
+    do {
+        var index = Math.floor(Math.random() * (eng.length - 1));
+        var answer = prompt(eng[index]);
+        if(rus[index] == answer) {
+            alert('You r cool')
+         } else {
+                alert('Go to school')
+            }
+        } while(confirm('continue?'));
+
+        }
+
+test();
